d8b8c16668333b488a23cbc0fe94bfdc44087681
Valter Oliveira <ubi00179@gmail.com>
2024-03-13 19:17:32 -0300

    feat(age-calculator): implement function to handle birth data entries
    -handle entries of user's year, month and day of birth

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,54 @@ function handleEmptyInput(input) {
     fieldBoxAlert(fieldBox, false);
     return true;
 }
+
+function handleInput(input) {
+    let isValid = true;
+
+    const regex = /^[0-9]+$/;
+    const { value: inputValue } = input;
+    const fieldBox = input.parentNode;
+
+    if (input.id === 'userBirthDay') {
+        if (!regex.test(inputValue) || inputValue < 1 || inputValue > 31) {
+            const alert = createAlert('Must be a valid day');
+            input.insertAdjacentElement('afterend', alert);
+
+            fieldBoxAlert(fieldBox, true);
+            isValid = false;
+        }
+    }
+
+    if (input.id === 'userBirthMonth') {
+        if (!regex.test(inputValue) || inputValue < 1 || inputValue > 12) {
+            const alert = createAlert('Must be a valid month');
+            input.insertAdjacentElement('afterend', alert);
+
+            fieldBoxAlert(fieldBox, true);
+            isValid = false;
+        }
+    }
+
+    if (input.id === 'userBirthYear') {
+        const currentDate = new Date();
+        const year = currentDate.getFullYear();
+
+        if (!regex.test(inputValue) || inputValue < 1) {
+            const alert = createAlert('Must be a valid year');
+            input.insertAdjacentElement('afterend', alert);
+
+            fieldBoxAlert(fieldBox, true);
+            isValid = false;
+        }
+
+        if (inputValue > year) {
+            const alert = createAlert('Must be in the past');
+            input.insertAdjacentElement('afterend', alert);
+
+            fieldBoxAlert(fieldBox, true);
+            isValid = false;
+        }
+    }
+
+    return isValid;
+}
